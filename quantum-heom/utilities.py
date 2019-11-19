c6a325cba1c45a97ff0fbaf36b82224abe2a5132
Joe Abbott <jwa7@uw.edu>
2019-11-19 09:55:58 +0000

    useful linalg fxns

---
+++ quantum-heom/utilities.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def get_trace_matrix_squared(matrix: np.array) -> complex:
+
+    """
+    Returns the trace of the square of an input matrix.
+
+    Parameters
+    ----------
+    matrix : array of array of complex
+        The input matrix whose square trace will be evaluated.
+
+    Returns
+    -------
+    trace_sq : complex
+        The trace of the square of the input matrix.
+    """
+
+    assert matrix.shape[0] == matrix.shape[1], 'Input matrix must be square.'
+
+    return np.trace(np.matmul(matrix, matrix))
