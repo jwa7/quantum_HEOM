3aaa8d489a94d0207b65579f5f8bffd35cc1f15f
Joe Abbott <jwa7@uw.edu>
2019-12-02 15:32:36 +0000

    Fxn to calculate (anti)commutator

--- quantum-heom/utilities.py
+++ quantum-heom/utilities.py
@@ -20,3 +20,41 @@ def get_trace_matrix_squared(matrix: np.array) -> complex:
     assert matrix.shape[0] == matrix.shape[1], 'Input matrix must be square.'
 
     return np.trace(np.matmul(matrix, matrix))
+
+
+def get_commutator(A: np.array, B: np.array, anti: bool = False) -> complex:
+
+    """
+    Returns either the commutator:
+
+    .. math::
+        [A, B] = AB - BA
+
+    or the anti-commutator:
+
+    .. math::
+        {A, B} = AB + BA
+
+    of 2 square matrices A and B.
+
+    Parameters
+    ----------
+    A, B : array of array of complex
+        Input square matrices for which the (anti) commutator will be
+        calculated.
+    anti : bool
+        If True calculates the anti-commutator of A and B, otherwise
+        calculates just the commutator. Default value is False.
+
+    Returns
+    -------
+    array of array of complex
+        The (anti) commutator of A and B.
+    """
+    assert A.shape[0] == A.shape[1], 'Input matrix must be square.'
+    assert B.shape[0] == B.shape[1], 'Input matrix must be square.'
+
+    if anti:
+        return np.matmul(A, B) + np.matmul(B, A)
+
+    return np.matmul(A, B) - np.matmul(B, A)
