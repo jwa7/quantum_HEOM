59221c7a7ab9f044e42df85a62e9ddef7ba78136
Joe Abbott <jwa7@uw.edu>
2019-12-05 15:08:14 +0000

    adding stub unit tests for utilities functions

---
+++ tests/test_utilities.py
@@ -0,0 +1,50 @@
+"""Tests the functions contained within utilities.py"""
+
+import utilities as util
+
+
+@pytest.fixture
+def array_2d():
+
+    """
+    Returns a 2D numpy array.
+    """
+
+
+@pytest.fixture
+def init_rho():
+
+    """
+    Returns an initial density matrix for a simple open quantum
+    system, with N=2.
+    """
+
+    raise NotImplementedError
+
+
+
+def test_trace_matrix_squared():
+
+    """
+    Tests that the correct value for the matrix squared is returned.
+    """
+
+    raise NotImplementedError
+
+
+def test_commutator():
+
+    """
+    Tests that the correct commutator of A and B is returned.
+    """
+
+    raise NotImplementedError
+
+
+def test_anti_commutator():
+
+    """
+    Tests that the correct anti-commutator of A and B is returned.
+    """
+
+    raise NotImplementedError
