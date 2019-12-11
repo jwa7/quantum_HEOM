7948077268cfa3394e0a202fe4e7ae3b86c704e7
Joe Abbott <jwa7@uw.edu>
2019-12-11 18:26:57 +0000

    removing fxns that were put as properties of QuantumSystem

--- quantum_heom/hamiltonian.py
+++ quantum_heom/hamiltonian.py
@@ -6,69 +6,6 @@ import numpy as np
 import QuantumSystem
 
 
-def build_H_nearest_neighbour(system: QuantumSystem) -> np.array:
-
-    N = system.sites
-    # Change into atomic units if appropriate
-    hbar = 1 if system.atomic_units else constants.hbar
-    # Build base Hamiltonian for linear system
-    H = np.eye(N, k=-1, dtype=complex) + np.eye(N, k=1, dtype=complex)
-    # Encorporate interaction (between 1st and Nth sites) for cyclic systems
-    if system.interaction_model == 'nearest_neighbour_cyclic':
-        H[0][N-1] = 1
-        H[N-1][0] = 1
-
-    return H * hbar
-
-
-# def build_H_nearest_neighbour(N: int, cyclic: bool = True,
-#                               au: bool = True) -> np.array:
-#
-#     """
-#     Builds an N x N square Hamiltonian matrix that represents
-#     interactions in the nearest-neighbour model (i.e. between
-#     adjacent sites) in either a linear or cyclic connection of
-#     sites.
-#
-#     Parameters
-#     ----------
-#     N : int
-#         The number of sites in the quantum system.
-#     cyclic : bool
-#         True if the system has site connections closed into a
-#         cyclic loop. False if system is linear; i.e. termini
-#         at sites 1 and N. Default: True.
-#     au : bool
-#         If True sets hbar = 1 so that elements of H are 0 or 1.
-#         If False, elements are scaled by a factor of hbar.
-#         Default value is True.
-#
-#     Returns
-#     -------
-#     H : array of array of int/float
-#         An N x N array containing binary representation for interactions
-#         between ith and jth sites; where i, j in 1, ..., N .
-#     """
-#
-#     assert N > 0, 'Must pass N as a positive integer.'
-#
-#     # Change into atomic units if appropriate
-#     hbar = 1 if au else constants.hbar
-#     # Deal with easy special cases
-#     if N == 1:
-#         return np.array([1])
-#     if cyclic and N == 2:  # Cyclic and linear systems give same H for N = 2
-#         cyclic = False
-#     # Build base Hamiltonian for linear system
-#     H = np.eye(N, k=-1, dtype=complex) + np.eye(N, k=1, dtype=complex)
-#     # Encorporate interaction (between 1st and Nth sites) for cyclic systems
-#     if cyclic:
-#         H[0][N-1] = 1
-#         H[N-1][0] = 1
-#
-#     return H * hbar
-
-
 def pad_H_zero_exciton_gs(H: np.array) -> np.array:
 
     """
@@ -94,30 +31,3 @@ def pad_H_zero_exciton_gs(H: np.array) -> np.array:
         H = np.insert(H, 0, np.zeros(dim, dtype=complex), axis=axis)
 
     return H
-
-
-def build_H_superop(H: np.array) -> np.array:
-
-    """
-    Builds an (N^2 x N^2) commutation superoperator from the (N x N)
-    interaction Hamiltonian (H) for an open quantum system, given by:
-
-    .. math::
-        H_{sup} = -i(H \\otimes I - I \\otimes H^T)
-
-    where I is the identity of size (N x N).
-
-    Parameters
-    ----------
-    H : array of array of complex
-        The interaction Hamiltonian for the open quantum system.
-
-    Returns
-    -------
-    H_sup : array of array of complex
-        The (N^2 x N^2) commutation superoperator for the interaction
-        Hamiltonian.
-    """
-
-    return (-1.0j * (np.kron(H, np.identity(H.shape[0]))
-                     - np.kron(np.identity(H.shape[0]), H.T.conjugate())))
