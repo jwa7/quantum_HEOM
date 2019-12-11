ec4a4df4a45c90bc29d8accd3bc1753e474ba35e
Joe Abbott <jwa7@uw.edu>
2019-12-11 18:34:27 +0000

    removing unnecessary tests, adding new one

--- tests/test_hamiltonian.py
+++ tests/test_hamiltonian.py
@@ -6,66 +6,23 @@ import pytest
 import quantum_heom.hamiltonian as ham
 
 
-@pytest.mark.parametrize('N, expected', [(1, np.array([1])),
-                                         (2, np.array([[0, 1], [1, 0]])),
-                                         (3, np.array([[0, 1, 1],
-                                                       [1, 0, 1],
-                                                       [1, 1, 0]])),
-                                         (4, np.array([[0, 1, 0, 1],
-                                                       [1, 0, 1, 0],
-                                                       [0, 1, 0, 1],
-                                                       [1, 0, 1, 0]]))])
-def test_build_H_nearest_neighbour_cyclic(N, expected):
+@pytest.mark.parametrize('input, exp', [(np.array([[0, 1],
+                                                   [1, 0]]),
+                                         np.array([[0, 0, 0],
+                                                   [0, 0, 1],
+                                                   [0, 1, 0]])),
+                                        (np.array([[1, 2, 3],
+                                                   [4, 5, 6],
+                                                   [7, 8, 9]]),
+                                         np.array([[0, 0, 0, 0],
+                                                   [0, 1, 2, 3],
+                                                   [0, 4, 5, 6],
+                                                   [0, 7, 8, 9]]))])
+def test_pad_hamiltonian_zero_exciton_gs(input, exp):
 
     """
-    Tests that the correct Hamiltonian for a cyclic system
-    in the nearest neighbour model is constructed.
+    Tests that input N x N Hamiltonians are padded correctly
+    with the elements in the first row and column equal to zero.
     """
 
-    assert np.all(ham.build_H_nearest_neighbour(N, cyclic=True) == expected)
-
-
-@pytest.mark.parametrize('N, expected', [(1, np.array([1])),
-                                         (2, np.array([[0, 1], [1, 0]])),
-                                         (3, np.array([[0, 1, 0],
-                                                       [1, 0, 1],
-                                                       [0, 1, 0]])),
-                                         (4, np.array([[0, 1, 0, 0],
-                                                       [1, 0, 1, 0],
-                                                       [0, 1, 0, 1],
-                                                       [0, 0, 1, 0]]))])
-def test_build_H_nearest_neighbour_linear(N, expected):
-
-    """
-    Tests that the correct Hamiltonian for a linear system
-    in the nearest neighbour model is constructed.
-    """
-
-    assert np.all(ham.build_H_nearest_neighbour(N, cyclic=False) == expected)
-
-
-@pytest.mark.parametrize('N', [0, -1])
-def test_build_H_nearest_neighbour_errors(N):
-
-    """
-    Tests that the correct Hamiltonian for a linear system
-    in the nearest neighbour model is constructed.
-    """
-
-    with pytest.raises(AssertionError):
-        ham.build_H_nearest_neighbour(N)
-
-
-@pytest.mark.parametrize('H, expected', [(np.array([[0, 1], [1, 0]]),
-                                          np.array([[0, -1, 1, 0],
-                                                    [-1, 0, 0, 1],
-                                                    [1, 0, 0, -1],
-                                                    [0, 1, -1, 0]]) * -1.0j)])
-def test_build_H_superop(H, expected):
-
-    """
-    Tests, given an input N x N Hamiltonian, that the correct
-    Hamiltonian superoperator is constructed.
-    """
-
-    assert np.all(ham.build_H_superop(H) == expected)
+    assert np.all(ham.pad_hamiltonian_zero_exciton_gs(input) == exp)
