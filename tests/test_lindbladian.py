c901a6fa2a190945231e087a50d2c4cf1e109361
Joe Abbott <jwa7@uw.edu>
2019-12-13 09:52:23 +0000

    updating docstrings and import aliases for consistency

--- tests/test_lindbladian.py
+++ tests/test_lindbladian.py
@@ -1,4 +1,5 @@
-"""Tests the functions that build the lindbladian operators."""
+"""Tests the functions that build the dephaising lindbladian operators
+and lindbladian superoperator."""
 
 import numpy as np
 import pytest
@@ -22,27 +23,27 @@ GAMMA = 0.15
                                                              [0, 0, 0, 0, 0],
                                                              [0, 0, 0, 0, 0]]))
                                             ])
-def test_build_lindblad_operator(N, j, expected):
+def test_dephasing_lindblad_op(N, j, expected):
 
     """
-    Tests that the lindblad operator P_j for a given site
-    j in {1, ... , N} is constructed correctly.
+    Tests that the dephasing lindblad operator P_j for a given
+    site j in {1, ... , N} is constructed correctly.
     """
 
-    assert np.all(lind.build_lindblad_operator(N, j) == expected)
+    assert np.all(lind.dephasing_lindblad_op(N, j) == expected)
 
 
 @pytest.mark.parametrize('N, j', [(5, 0), (0, 3)])
-def test_build_lindblad_operator_errors(N, j):
+def test_dephasing_lindblad_op_errors(N, j):
 
     """
     Tests that the correct error is raised when passing invalid
     total sites (N) and site numbers (j) to the function
-    build_lindblad_operator.
+    dephasing_lindblad_op.
     """
 
     with pytest.raises(AssertionError):
-        lind.build_lindblad_operator(N, j)
+        lind.dephasing_lindblad_op(N, j)
 
 
 @pytest.mark.parametrize('N, exp', [(2, np.array([[0, 0, 0, 0],
@@ -59,11 +60,12 @@ def test_build_lindblad_operator_errors(N, j):
                                                   [0, 0, 0, 0, 0, 0, 0, -1, 0],
                                                   [0, 0, 0, 0, 0, 0, 0, 0, 0]])
                                         * GAMMA)])
-def test_build_lindbladian_superop(N, exp):
+def test_lindbladian_superop(N, exp):
 
     """
-    Tests that the correct Lindbladian dephasing superoperator
+    Tests that the correct dephasing Lindbladian superoperator
     is constructed for the number of sites N.
     """
 
-    assert np.all(lind.build_lindbladian_superop(N, GAMMA) == exp)
+    assert np.all(lind.lindbladian_superop(N, GAMMA,
+                                           model='dephasing lindblad') == exp)
