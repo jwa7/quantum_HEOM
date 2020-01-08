0533267a847b1d7193091748a1264b8fcc739669
Joe Abbott <jwa7@uw.edu>
2020-01-08 18:56:30 +0000

    coding measure of non-markovianity

--- quantum_heom/utilities.py
+++ quantum_heom/utilities.py
@@ -1,9 +1,12 @@
 """Contains general use utility functions."""
 
+import datetime
+from itertools import permutations, product
+
 import numpy as np
 
 
-def get_trace_matrix_squared(matrix: np.array) -> complex:
+def trace_matrix_squared(matrix: np.array) -> complex:
 
     """
     Returns the trace of the square of an input matrix.
@@ -24,8 +27,35 @@ def get_trace_matrix_squared(matrix: np.array) -> complex:
 
     return np.trace(np.matmul(matrix, matrix))
 
+def trace_distance(A: np.array, B: np.array) -> float:
 
-def get_commutator(A: np.array, B: np.array, anti: bool = False) -> complex:
+    """
+    Returns the measure of non-Markovianity of the system as
+    defined in H.-P. BREUER, E.-M. LAINE, AND J. PIILO, Measure
+    for the Degree of Non-Markovian Behavior of Quantum Processes
+    in Open Systems, Phys. Rev. Lett., 103 (2009), p. 210401,
+    given by:
+
+    .. math::
+        0.5 tr(|A - B|)
+
+    where $|A| = (A^\\dagger A)^{frac{1}{2}}$ and $\\A$ is
+    the density matrix at time t, and $\\B$ is the equilibrium
+    density matrix; either the thermal equilibrium state for
+    thermal-based approaches, or the maximally mixed state for
+    dephasing models in the infinite temperature limit.
+
+    Returns
+    ------
+    float
+        The trace distance of the density matrix relative to the
+        equilibrium state.
+    """
+
+    return 0.5 * np.trace(np.sqrt(np.matmul((A - B).T.conjugate(), (A - B))))
+
+
+def commutator(A: np.array, B: np.array, anti: bool = False) -> complex:
 
     """
     Returns either the commutator:
@@ -62,3 +92,91 @@ def get_commutator(A: np.array, B: np.array, anti: bool = False) -> complex:
         return np.matmul(A, B) + np.matmul(B, A)
 
     return np.matmul(A, B) - np.matmul(B, A)
+
+def elements_from_str(sites: int, elements: str) -> list:
+
+    """
+    Generates a list of elements of the density matrix from a
+    string representation. For instance, if elements='all' and
+    sites=3, this function will return ['11', '12', '13', '21',
+    '22', '23', '31', '32', '33'], if elements='diagonals' will
+    return ['11', '22', '33'], and if elements='off-diagonals'
+    will return ['12', '13', '21', '23', '31', '32'].
+
+    Parameters
+    ----------
+    sites : int
+        The number of sites in the Open Quantum System.
+    elements : str
+        The numerical string representations of the elements of
+        the square density matrix to return. Either 'all',
+        'diagonals', or 'off-diagonals'.
+
+    Returns
+    -------
+    list
+        A list of numerical string representations of denisty
+        matrix elements.
+    """
+
+    # Check elements input
+    if isinstance(elements, list):
+        assert len(elements) <= sites ** 2, (
+            'The number of elements plotted must be a positive integer less'
+            ' than or equal to the number of elements in the density matrix.')
+        for element in elements:
+            try:
+                int(element)
+            except ValueError:
+                raise ValueError('Invalid format of string representation of'
+                                 ' density matrix element.')
+        return elements
+    elif isinstance(elements, str):
+        assert elements in ['all', 'diagonals', 'off-diagonals'], (
+            'Must choose from "all", "diagonals", or "off-diagonals".')
+        if elements == 'all':
+            return [str(i) + str(j)
+                    for i, j in product(range(1, sites + 1), repeat=2)]
+        elif elements == 'diagonals':
+            return [str(i) + str(i) for i in range(1, sites + 1)]
+        else:  # off-diagonals
+            return [str(i) + str(j)
+                    for i, j in permutations(range(1, sites + 1), 2)]
+    else:
+        raise ValueError('elements argument passed as invalid value.')
+
+def date_stamp():
+
+    """
+    Creates a unique time stamp for the current time to the nearest
+    100th of a second that contains no other character than digits
+    0~9, i.e.'2019-05-17 17:04:19.92' ---> '2019051717041992'.
+
+    Creates a numerical string representation of today's date,
+    containing only digits 0-9 and '_' characters. I.e. on the 29th
+    Jan 2020 this function would return '2020_01_29'.
+
+    Returns:
+    -----
+    time_stamp : str
+        A unique numerical string of the current date.
+    """
+
+    return str(datetime.datetime.now().date()).replace('-', '_')
+
+def time_stamp():
+
+    """
+    Creates a unique time stamp for the current time to the nearest
+    100th of a second that contains no other character than digits
+    0~9, i.e.'2019-05-17 17:04:19.92' ---> '2019051717041992'.
+
+    Returns:
+    -----
+    time_stamp : str
+        A unique numerical string of the current time to the
+        nearest 100th of a second.
+    """
+
+    return (''.join([i for i in str(datetime.datetime.now())
+                     if i not in [' ', '-', '.', ':']])[:-4])
