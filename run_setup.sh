622c61c9c572c84d3f1719146bba6e07c8c9a273
Joe Abbott <jwa7@uw.edu>
2020-01-29 11:22:41 +0000

    altering ordering of setup

--- run_setup.sh
+++ run_setup.sh
@@ -1,18 +1,19 @@
 # Define the name of the virtual environment
 NAME=qHEOM
 
-# Create and enter the conda environment
+# Create the conda virtual environment
 echo 'CREATING VIRTUAL ENVIRONEMENT...'
 yes | conda env create -f environment.yml -n $NAME
 sleep 1s
-
-echo '...ENTERING VIRTUAL ENVIRONMENT...'
-conda activate heom
-sleep 2s
 
 # Configure the environment into a kernel for use in ipython
 echo 'CONFIGURING IPYTHON KERNEL...'
 ipython kernel install --user --name=$NAME
 sleep 1s
+
+# Enter the virtual environment
+echo '...ENTERING VIRTUAL ENVIRONMENT...'
+conda activate heom
+sleep 2s
 
 echo 'DONE.'
