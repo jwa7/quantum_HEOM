1f32178bf247f619ab3041c6dd291628734151af
Joe Abbott <jwa7@uw.edu>
2020-01-29 13:52:05 +0000

    fixing setup bug

--- run_setup.sh
+++ run_setup.sh
@@ -1,10 +1,54 @@
-# Define the name of the virtual environment
-NAME=qHEOM
+# Define environment name
+NAME=qheom
 
-# Create the conda virtual environment
-echo 'CREATING VIRTUAL ENVIRONMENT...'
-yes | conda env create -f environment.yml -n $NAME python=3.7 
+# Welcome
+echo '----------------------------------------------------------------'
+echo '                    WELCOME TO QUANTUM_HEOM                     '
+echo '----------------------------------------------------------------'
+echo '                                        Author: JOSEPH W. ABBOTT'
 sleep 1s
+echo '  quantum_HEOM can be used to plot the dynamics of open-quantum'
+echo '  systems using Lindblad-based and Hierarchical Equations of   ' 
+echo '  Motion (HEOM) approaches. '
+echo '----------------------------------------------------------------'
+echo ''
+sleep 1s
+echo 'START INSTALLATION:'
+echo ''
+
+# Create the virtual conda environment
+echo 'CREATING VIRTUAL ENVIRONMENT...'
+yes | conda create -n $NAME python=3.7
+echo 'DONE.'
+sleep 1s
+
+# Configure the enviornment as a ipython kernel
+echo 'CONFIGURING IPYTHON KERNEL...'
+ipython kernel install --user --name=$NAME
+echo 'DONE.'
+sleep 1s
+
+# Enter the environment
+echo 'ENTERING THE VIRTUAL ENVIRONMENT...'
+source activate $NAME
+echo 'DONE.'
+sleep 1s
+
+# Install packages
+echo 'INSTALLING PACKAGES...'
+yes | conda install qutip ipython jupyter matplotlib numpy scipy cython pytest
+echo 'DONE.'
+sleep 1s
+
+# Run tests
+echo 'RUNNING TESTS...'
+# pytest --v tests/
+echo 'DONE.'
+
+echo 'PROCESS COMPLETE.'
+echo 'ENTER THE FOLLOWING COMMANDS TO LAUNCH THE INTERACTIVE TUTORIAL:'
+echo '             cd tutorials'
+echo '             jupyter notebook quantum_HEOM_tutorial.ipynb &'
 
 # Configure the environment into a kernel for use in ipython
 echo 'CONFIGURING IPYTHON KERNEL...'
