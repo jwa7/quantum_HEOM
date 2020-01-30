4a84dca5dbd443fce0eb043e9c0c134cf014c180
Joe Abbott <jwa7@uw.edu>
2020-01-30 16:18:16 +0000

    cleaning up code, removing redundancies

--- quantum_heom/lindbladian.py
+++ quantum_heom/lindbladian.py
@@ -2,88 +2,175 @@
 thermalising (super)operators."""
 
 from itertools import permutations, product
-import math
-
-from scipy import constants, linalg
 import numpy as np
 
 import utilities as util
-
-TEMP_INDEP_MODELS = ['simple', 'local dephasing lindblad']
-TEMP_DEP_MODELS = ['local thermalising lindblad',
-                   'global thermalising lindblad',
-                   'HEOM']
 
 MODELS = ['local dephasing lindblad',
           'global thermalising lindblad',
           'local thermalising lindblad']
 
 
-def dephasing_lindblad_op(sites: int, site_j: int) -> np.array:
+def loc_deph_lindblad_op(dims: int, site_j: int) -> np.array:
 
     """
     Builds an N x N matrix that contains a single non-zero element
     (of value 1) at element (site_j, site_j), where N is the number
-    of sites in the system. Used in constructing the dephasing
-    lindbladian superoperator.
+    of sites in the system. Constructs the local dephasing Lindblad
+    operator in the site basis.
 
     Parameters
     ----------
-    sites : int
-        The number of sites in the open quantum system and
-        hence the dimension of the lindblad operator.
+    dims : int
+        The dimension (i.e. number of sites) of the open quantum
+        system and hence the dimension of the Lindblad operator.
     site_j : int
-        The site number for which to build the lindblad operator.
+        The site index for which to build the Lindblad operator,
+        using zero-indexing.
 
     Returns
     -------
-    lindblad_operator : array of array of int
-        The lindblad operator corresponding to jth site in the quantum
-        system.
+    l_op : np.array
+        2D square array corresponding to the local dephasing
+        Lindblad operator for the jth site in the quantum system.
     """
 
-    assert site_j > 0, 'The site number must be a positive integer'
-    assert site_j <= sites, ('The site number cannot be larger than the total'
-                             ' number of sites')
+    assert site_j >= 0, 'The site number must be a positive integer'
+    assert site_j <= dims, ('The site number cannot be larger than the total'
+                            ' number of sites')
 
-    lindblad_operator = np.zeros((sites, sites), dtype=complex)
-    lindblad_operator[site_j - 1][site_j - 1] = 1.+0.j
+    l_op = np.zeros((dims, dims), dtype=complex)
+    l_op[site_j - 1][site_j - 1] = 1.+0.j
+    return l_op
 
-    return lindblad_operator
-
-def thermalising_lindblad_op(sites: int, state_a: int, state_b: int):
+def glob_therm_lindblad_op(dims: int, state_a: int, state_b: int):
 
     """
     Builds an N x N matrix that contains a single non-zero element
-    (of value 1) at element (state_b, state_a), where N is the number
-    of sites in the system. Used in constructing the thermalising
-    lindbladian superoperator.
+    (of value 1) at element (state_b, state_a), where N is the
+    number of sites in the system, and state_a and state_b are
+    indices for different eigenstates a and b of the system
+    Hamiltonian. Corresponds to the global thermalising Lindblad
+    operator A in the eigenstate basis, given by:
+
+    .. math::
+        A = \\ket{b} \\bra{a}
 
     Parameters
     ----------
-    sites : int
-        The number of sites in the open quantum system and
-        hence the dimension of the lindblad operator.
+    dims : int
+        The dimensions (i.e. number of states) of the open quantum
+        system and hence the dimension of the lindblad operator.
     state_a : int
-        The number of the state receiving exciton population.
+        The index of the state receiving exciton population, using
+        zero-indexing.
     state_b : int
-        The number of the state transferring exiton population.
+        The index of the state transferring exiton population,
+        using zero-indexing.
+
+    Returns
+    -------
+    l_op = np.array
+        2D array corresponding to the global thermalising Lindblad
+        operator in the eigenstate basis.
     """
 
     assert state_a >= 0, 'The state number must be a non-negative integer'
     assert state_b >= 0, 'The state number must be a non-negative integer'
-    assert state_a <= sites - 1, ('The state number cannot be larger than the'
-                                  ' total number of sites')
-    assert state_b <= sites - 1, ('The state number cannot be larger than the'
-                                  ' total number of sites')
+    assert state_a <= dims - 1, ('The state number cannot be larger than the'
+                                 ' total number of states')
+    assert state_b <= dims - 1, ('The state number cannot be larger than the'
+                                 ' total number of states')
+    assert state_a != state_b, ('The global thermalising Lindblad operator is'
+                                ' only constructed for pairs of different'
+                                ' eigenstates.')
 
-    ket_b = np.zeros(sites, dtype=float)
-    bra_a = np.zeros(sites, dtype=float).reshape(sites, 1)
+    ket_b = np.zeros(dims, dtype=complex)
+    bra_a = np.zeros(dims, dtype=complex)
     ket_b[state_b] = 1.
     bra_a[state_a] = 1.
-    lindblad_op = np.outer(ket_b, bra_a)
+    l_op = np.outer(ket_b, bra_a)
+    return l_op
 
-    return lindblad_op
+def loc_therm_lindblad_op(eigv: np.array, eigs: np.array, unique: float,
+                          site_m: int) -> np.array:
+
+    """
+    Builds an N x N matrix (where N is the number of sites/states
+    in the open quantum system) corresponding to the local
+    thermalising Lindblad operator A for a given unique eigenstate
+    frequency gap \\omega and site m. Is constructed by summation
+    over all eigenstate frequency gaps \\omega_{ij} that correspond
+    to the unique frequency gap \\omega, given by:
+
+    .. math::
+        A = \\sum_{\\omega_{ij}=\\omega}
+            c_m^*(j) c_m(i) \\ket{j} \\bra{i}
+
+    Parameters
+    ----------
+    eigv : np.array
+        A 1D array of eigenvalues of the system Hamiltonian, where
+        the ith element corresponds to the ith eigenstate.
+    eigs : np.array
+        A 2D array of eigenstates of the system Hamiltonian, where
+        the ith column corresponds to the ith eigenstate.
+    unique : float
+        The unique eigenstate frequency gap for which the local
+        thermalising Lindblad operator will be constructed.
+    site_m : int
+        The index of the site for which the local thermalising
+        Lindblad operator will be constructed, using zero-indexing.
+
+    Returns
+    -------
+    l_op : np.array
+        2D array corresponding to the global thermalising Lindblad
+        operator in the eigenstate basis.
+    """
+
+    dims = len(eigv)
+    l_op = np.zeros((dims, dims), dtype=complex)
+    for idx_i, idx_j in product(range(dims), repeat=2):
+        omega_i, omega_j = eigv[idx_i], eigv[idx_j]
+        state_i, state_j = eigs[:, idx_i], eigs[:, idx_j]
+        if omega_i - omega_j == unique:
+            l_op += (state_j[site_m].conjugate() * state_i[site_m]
+                     * np.outer(state_j, state_i))
+    return l_op
+
+def lindblad_superop_sum_element(l_op: np.array) -> np.array:
+
+    """
+    For a Lindbladian superoperator that is formed by summation
+    over index \\alpha, this function constructs the Lindbladian
+    superoperator for a particular value of this index from the
+    corresponding Lindblad operator, A. This is given by:
+
+    .. math::
+        A^* \\otimes A - 0.5
+        ((A^{\\dagger} A)^* \\otimes I + I \\otimes A^{\\dagger} A)
+
+    Parameters
+    ----------
+    l_op : np.array
+        The Lindblad operator A for the given Lindblad model.
+
+    Returns
+    -------
+    np.array
+        The N^2 x N^2 superoperator (where N x N is the dimension
+        of the Lindblad operator) for the specific index at which
+        the Lindblad operator has been constructed.
+    """
+
+    assert l_op.shape[0] == l_op.shape[1], 'Lindblad operator must be square.'
+
+    l_op_dag = l_op.T.conjugate()
+    iden = np.eye(l_op.shape[0])
+    return (np.kron(l_op.conjugate(), l_op)
+            - 0.5 * (np.kron(np.matmul(l_op_dag, l_op).conjugate(), iden)
+                     + np.kron(iden, np.matmul(l_op_dag, l_op))))
 
 def lindbladian_superop(qsys) -> np.array:
 
@@ -96,20 +183,10 @@ def lindbladian_superop(qsys) -> np.array:
     qsys.dynamics_model. The local dephasing lindbladian is
     given by:
 
-    .. math::
-        L_{deph} = \\sum_j (P_j \\otimes P_j
-                            - \\frac{1}{2}
-                              (P_j^{\\dagger} P_j \\otimes \\mathds(I)
-                               - \\mathds{I} \\otimes P_j^{\\dagger} P_j))
 
-    and the global thermalising lindbladian is given by:
-
-    .. math::
-        L_{therm} = \\sum_{a \\notequal b}
-                    (P_{ab} \\otimes P_{ba}
-                     - \\frac{1}{2}
-                       (P_{bb}^{\\dagger} P_{bb} \\otimes \\mathds{I}
-                        - \\mathds{I} \\otimes P_{bb}^{\\dagger} P_{bb}))
+        where k(\\omega) is the rate of population transfer between
+        eigenstates that have a frequency gap that corresponds to
+        the unique frequency \\omega.
 
     Parameters
     ----------
@@ -125,111 +202,43 @@ def lindbladian_superop(qsys) -> np.array:
         units of rad s^-1.
     """
 
+    dims = qsys.sites
     hamiltonian = qsys.hamiltonian
-    lindbladian = np.zeros((qsys.sites ** 2, qsys.sites ** 2), dtype=complex)
-    id = np.identity(qsys.sites, dtype=complex)
+    eigv, eigs = util.eigv(hamiltonian), util.eigs(hamiltonian)
+    lindbladian = np.zeros((dims ** 2, dims ** 2), dtype=complex)
 
     if qsys.dynamics_model == 'local dephasing lindblad':
-
-        for site_j in range(1, qsys.sites + 1):
-            site_j_op = dephasing_lindblad_op(qsys.sites, site_j)
-            lind_j_op = (np.kron(site_j_op.T.conjugate(), site_j_op)
-                         - 0.5 * (np.kron(id, np.matmul(site_j_op.T.conjugate(),
-                                                        site_j_op))
-                                  + np.kron(np.matmul(site_j_op.T.conjugate(),
-                                                      site_j_op),
-                                            id)))
-            lindbladian += lind_j_op
-        # decay rate given in units of s^-1
-        return lindbladian * qsys.decay_rate # rad s^-1
+        # Linblad operator evaluated for each site, in the site basis.
+        deph_rate = qsys.decay_rate
+        for site_j in range(dims):
+            l_op = loc_deph_lindblad_op(dims, site_j)
+            indiv_superop = lindblad_superop_sum_element(l_op)
+            lindbladian += indiv_superop
+        return deph_rate * lindbladian
 
     if qsys.dynamics_model == 'global thermalising lindblad':
-
-        eigv, eigs = util.eigv(hamiltonian), util.eigs(hamiltonian)  # rad s^-1
-        omega = eigv - eigv.reshape(qsys.sites, 1)
-        # Iterate over all pairs of different states (a notequal b)
-        for state_a, state_b in permutations(range(0, qsys.sites), 2):
-
-            # L_ab = ket(b) x bra(a)   (outer product |b><a|)
-            # Construct in the eigenbasis
-            state_ab_op = thermalising_lindblad_op(qsys.sites, state_a, state_b)
-            # state_ab_op = np.outer(eigs[:, state_b], eigs[:, state_a])
-            omega_ab = eigv[state_a] - eigv[state_b]  # rad s^-1
-            k_ab = rate_constant_redfield(qsys, omega_ab)  # rad s^-1
-            if omega_ab.round(decimals=0) == 0. or k_ab == 0:
-                continue
-            lind_ab_op = k_ab * (np.kron(state_ab_op.conjugate(), state_ab_op)
-                                 - 0.5
-                                 * (np.kron(np.matmul(state_ab_op.T.conjugate(),
-                                                      state_ab_op).conjugate(),
-                                            id)
-                                    + np.kron(id,
-                                              np.matmul(state_ab_op.T.conjugate(),
-                                                        state_ab_op))))
-            lindbladian += lind_ab_op #/ (2 * np.pi)  # 2pi is a constant
-        # Transform back to site basis
-        lindbladian = util.lspace_basis_change(lindbladian, eigs)
-        return lindbladian  # rad s^-1
+        # Lindblad operator constructed for each pair of different
+        # eigenstates, in the eigenbasis.
+        for state_a, state_b in permutations(range(dims), 2):
+            omega_a, omega_b = eigv[state_a], eigv[state_b]
+            k_ab = rate_constant_redfield(qsys, omega_a - omega_b)
+            l_op = glob_therm_lindblad_op(dims, state_a, state_b)
+            indiv_superop = lindblad_superop_sum_element(l_op)
+            lindbladian += k_ab * indiv_superop
+        return lindbladian
 
     if qsys.dynamics_model == 'local thermalising lindblad':
-
-        # lin = np.array([[-2.19156757e+13+0.j, 0.00000000e+00+0.j,
-        #                                  0.00000000e+00+0.j, 4.82639606e+13+0.j],
-        #                                 [0.00000000e+00+0.j, -3.50898181e+13+0.j,
-        #                                  0.00000000e+00+0.j, 0.00000000e+00+0.j],
-        #                                 [0.00000000e+00+0.j, 0.00000000e+00+0.j,
-        #                                  -3.50898181e+13+0.j, 0.00000000e+00+0.j],
-        #                                 [2.19156757e+13+0.j, 0.00000000e+00+0.j,
-        #                                  0.00000000e+00+0.j, -4.82639606e+13+0.j]])
-        # lin = lin / (2 * np.pi)
-        # return lin
-
-        eigenvalues, eigenstates = linalg.eig(hamiltonian)
-        # Generate matrix where element (i, j) gives frequency gap between
-        # eigenstates i and j, i.e. omega_ij = omega_j - omega_i in rad s^-1
-        omega = eigenvalues - eigenvalues.reshape(qsys.sites, 1)
-        # Generate a flat list of unique energy gap values, accounting for
-        # rounding errors with a decimal tolerance.
-        decimals = 0
-        unique = np.unique(omega.flatten().round(decimals=decimals))
-        for uniq in unique:  # iterate over unique freq gaps
-
-            # If the unique freq gap is zero skip this iteration as will be zero
-            k_uniq = rate_constant_redfield(qsys, uniq)  # rad s^-1
-            if k_uniq == 0:
-                continue
-            # Initialise operator that will be the cumsum of individual site
-            # operators at freq 'uniq', and iterate over sites.
-            lind = np.zeros((qsys.sites**2, qsys.sites**2), dtype=complex)
-            for site_m in range(1, qsys.sites + 1):
-
-                # Initialise operator for site_m at freq 'uniq', and iterate
-                # over eigenstates; find all freq gaps that equal 'uniq'
-                site_m_op = np.zeros((qsys.sites, qsys.sites), dtype=complex)
-                # for state_i, state_j in permutations(range(0, qsys.sites), 2):
-                for state_i, state_j in product(range(0, qsys.sites), repeat=2):
-                    # omega_ij = omega_j - omega_i
-                    omega_ij = omega[state_j][state_i]
-                    if omega_ij.round(decimals=decimals) == uniq:
-                        # site_m_coeff_j = eigenstates[state_j][site_m - 1]
-                        # site_m_coeff_i = eigenstates[state_i][site_m - 1]
-                        site_m_coeff_j = eigenstates[site_m - 1][state_j]
-                        site_m_coeff_i = eigenstates[site_m - 1][state_i]
-                        site_m_op += (site_m_coeff_j.conjugate()
-                                      * site_m_coeff_i
-                                      * np.outer(eigenstates[state_j],
-                                                 eigenstates[state_i]))
-
-                # Get lindblad superoperator for site_m at freq 'uniq'
-                lind += (np.kron(site_m_op.conjugate(), site_m_op)
-                         - 0.5 * (np.kron(np.matmul(site_m_op.T,
-                                                    site_m_op).conjugate(), id)
-                                  + np.kron(id,
-                                            np.matmul(site_m_op.T,
-                                                      site_m_op))))
-
-            lindbladian += k_uniq * lind  # rad s^-1
-        return lindbladian  # rad s^-1
+        # Lindblad operator evaluated for each pair (x, y), where x
+        # is a unique frequency gap between eigenstates of the Hamiltonian
+        # and y is a site in the quantum system.
+        gaps = eigv - eigv.reshape(dims, 1)
+        unique = np.unique(gaps.flatten())  # DECIMAL ROUNDING HERE?????
+        for unique, site_m in product(unique, range(dims)):
+            k_omega = rate_constant_redfield(qsys, unique)
+            l_op = loc_therm_lindblad_op(eigv, eigs, unique, site_m)
+            indiv_superop = lindblad_superop_sum_element(l_op)
+            lindbladian += k_omega * indiv_superop
+        return lindbladian
 
     raise NotImplementedError('Other lindblad dynamics models not yet'
                               ' implemented in quantum_HEOM. Choose from: '
@@ -335,7 +344,4 @@ def bose_einstein_distrib(qsys, omega: float):
 
     if omega == 0.:
         return 0.
-        # raise ValueError('Cannot evaluate the Bose-Einstein distribution at'
-        #                  ' a frequency of zero.')
-
     return 1. / (np.exp(qsys.hbar * omega / qsys.kT) - 1)  # dimensionless
