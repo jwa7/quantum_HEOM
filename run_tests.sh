6bf0fde54bbc65b0a1c16c90efc5b5c20121c504
Joe Abbott <jwa7@uw.edu>
2020-01-30 17:13:22 +0000

    fixing bug in running of unit tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,8 @@
+# Run unit tests with pytest
+# Warnings are disbaled as have been accounted for
 python -m pytest -vv tests/ --disable-pytest-warnings
+
+# Remove unnecessary files and directories created in setup
 rm -Rf tests/__pycache__
 rm -Rf quantum_heom/__pycache__
 rm -Rf quantum_heom/.ipynb_checkpoints
