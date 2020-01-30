8c9cd324366831fb470980d49ed92921e59d7813
Joe Abbott <jwa7@uw.edu>
2020-01-30 19:37:00 +0000

    fixing import errors

--- quantum_heom/__init__.py
+++ quantum_heom/__init__.py
@@ -3,6 +3,10 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                '..')))
+idx = os.getcwd().rfind('quantum_HEOM')
+ROOT_DIR = os.getcwd()[:idx] + 'quantum_HEOM'
+if ROOT_DIR not in sys.path:
+    sys.path.append(ROOT_DIR)
 
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
+#                                                 '..')))
