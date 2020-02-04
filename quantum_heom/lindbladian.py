edb548b17e025f442c0bf7c072b4be78b05c2ac5
Joe Abbott <jwa7@uw.edu>
2020-02-04 12:15:02 +0000

    compartmentalising thermal-bath related code

--- quantum_heom/lindbladian.py
+++ quantum_heom/lindbladian.py
@@ -2,9 +2,9 @@
 thermalising (super)operators."""
 
 from itertools import permutations, product
-from scipy import constants
 import numpy as np
 
+from quantum_heom import bath
 from quantum_heom import utilities as util
 
 LINDBLAD_MODELS = ['local dephasing lindblad',
@@ -174,9 +174,10 @@ def lindblad_superop_sum_element(l_op: np.ndarray) -> np.ndarray:
                      + np.kron(iden, np.matmul(l_op_dag, l_op))))
 
 def lindbladian_superop(dims: int, hamiltonian: np.ndarray,
-                        dynamics_model: str, deph_rate: float,
-                        cutoff_freq: float, scale_factor: float,
-                        temperature: float) -> np.ndarray:
+                        dynamics_model: str, deph_rate: float = None,
+                        cutoff_freq: float = None, scale_factor: float = None,
+                        temperature: float = None, spectral_density: str = None,
+                        exponent: float = None) -> np.ndarray:
 
     """
     Builds an (dims^2) x (dims^2) Lindbladian superoperator matrix
@@ -234,23 +235,28 @@ def lindbladian_superop(dims: int, hamiltonian: np.ndarray,
         return deph_rate * lindbladian
 
     # Check inputs for thermalising models
-    for var in [cutoff_freq, scale_factor, temperature]:
+    for var in [cutoff_freq, scale_factor, temperature, spectral_density]:
         assert var is not None, (
-            'Need to pass a cutoff frequency, scaling factor, and temperature'
-            ' for thermalising models.')
+            'Need to pass a cutoff frequency, scaling factor, temperature,'
+            ' and spectral density for thermalising models.')
+    if spectral_density == 'ohmic':
+        assert exponent is not None, 'Need to pass the Ohmic exponent.'
 
     if dynamics_model == 'global thermalising lindblad':
         # Lindblad operator constructed for each pair of different
         # eigenstates, in the eigenbasis.
         for state_a, state_b in permutations(range(dims), 2):
             omega_a, omega_b = eigv[state_a], eigv[state_b]
-            k_a_to_b = rate_constant_redfield(omega_a - omega_b,
-                                              cutoff_freq,
-                                              scale_factor,
-                                              temperature)
+            k_a_to_b = bath.rate_constant_redfield(omega_a - omega_b,
+                                                   cutoff_freq,
+                                                   scale_factor,
+                                                   temperature,
+                                                   spectral_density,
+                                                   exponent)
             l_op = glob_therm_lindblad_op(dims, state_a, state_b)
             indiv_superop = lindblad_superop_sum_element(l_op)
             lindbladian += k_a_to_b * indiv_superop
+        lindbladian = util.basis_change(lindbladian, eigs)
         return lindbladian
 
     if dynamics_model == 'local thermalising lindblad':
@@ -260,10 +266,12 @@ def lindbladian_superop(dims: int, hamiltonian: np.ndarray,
         gaps = eigv - eigv.reshape(dims, 1)
         unique = np.unique(gaps.flatten())  # NEED DECIMAL ROUNDING HERE?
         for unique, site_m in product(unique, range(dims)):
-            k_omega = rate_constant_redfield(unique,
-                                             cutoff_freq,
-                                             scale_factor,
-                                             temperature)
+            k_omega = bath.rate_constant_redfield(unique,
+                                                  cutoff_freq,
+                                                  scale_factor,
+                                                  temperature,
+                                                  spectral_density,
+                                                  exponent)
             l_op = loc_therm_lindblad_op(eigv, eigs, unique, site_m)
             indiv_superop = lindblad_superop_sum_element(l_op)
             lindbladian += k_omega * indiv_superop
@@ -272,144 +280,3 @@ def lindbladian_superop(dims: int, hamiltonian: np.ndarray,
     raise NotImplementedError('Other lindblad dynamics models not yet'
                               ' implemented in quantum_HEOM. Choose from: '
                               + str(LINDBLAD_MODELS))
-
-def rate_constant_redfield(omega: float, cutoff_freq: float,
-                           scale_factor: float, temperature: float) -> float:
-
-    """
-    Calculates the rate constant for population transfer
-    between states separated by a frequency gap omega. For instance,
-    for a frequency gap omega = omega_i - omega_j,
-
-    .. math::
-        k_{\\omega}
-            = 2 J(\\omega) (1 + 2 n(\\omega)
-
-    where $n(\\omega_{ab})$ is the Bose-Einstein distribution
-    between eigenstates a and b separated by energy
-    $\\omega_{ab}$ and $J(\\omega_{ab})$ is the spectral density
-    at frequency $\\omega_{ab}$.
-
-    Parameters
-    ----------
-    omega : float
-        The frequency of the energy gap between states i and j.
-        Has the form omega = omega_i - omega_j. Must be in units of
-        rad s^-1.
-    cutoff_freq : float
-        The cutoff frequency at which the spectral density
-        evaluates to 1 (or the scale_factor value if f not equal
-        to 1), in units of rad s^-1. Must be a non-negative float.
-    scale_factor : float
-        The factor by which the spectral density should be scaled
-        by. Should be passed in units of rad s^-1. Must be a
-        non-negative float.
-    temperature : float
-        The temperature at which the rate constant should be
-        evaluated, in units of Kelvin.
-    """
-
-    assert cutoff_freq >= 0., (
-        'The cutoff freq must be a non-negative float, in units of rad s^-1')
-    assert scale_factor >= 0., (
-        'The scaling factor must be a positive float, in units of rad s^-1')
-
-    if omega == 0:
-        # Using an asymmetric spectral density only evaluated for positive omega
-        # Therefore the spectral density and rate is 0 for omega <= 0.
-        return 0.
-    if cutoff_freq == 0:
-        return 0.  # avoids DivideByZero error.
-
-    spec_omega_ij = debye_spectral_density(omega, cutoff_freq, scale_factor)
-    spec_omega_ji = debye_spectral_density(-omega, cutoff_freq, scale_factor)
-    n_omega_ij = bose_einstein_distrib(omega, temperature)
-    n_omega_ji = bose_einstein_distrib(-omega, temperature)
-    return (2
-            * ((spec_omega_ij * (1 + n_omega_ij))
-               + (spec_omega_ji * n_omega_ji)
-              )
-           )
-
-def debye_spectral_density(omega: float, cutoff_freq: float,
-                           scale_factor: float) -> float:
-
-    """
-    Calculates the Debye spectral density at frequency omega, with
-    a given cutoff frequency omega_c and scale factor f. It is
-    normalised so that if omega=omega_c and f=1, the spectral
-    density evaluates to 1. Implements an asymmetric spectral
-    density, evaluating to zero for omega <= 0. It is given by:
-
-    .. math::
-        \\omega^2 J(\\omega_{ab})
-            = f \\frac{2\\omega_c\\omega}{(\\omega_c^2
-                                           + \\omega^2) \\omega^2}
-
-    Parameters
-    ----------
-    omega : float
-        The frequency at which the spectral density will be
-        evaluated, in units of rad s^-1. If omega <= 0, the
-        spectral density evaluates to zero.
-    cutoff_freq : float
-        The cutoff frequency at which the spectral density
-        evaluates to 1 (or the scale_factor value if f not equal
-        to 1), in units of rad s^-1. Must be a non-negative float.
-    scale_factor : float
-        The factor by which the spectral density should be scaled
-        by. Should be passed in units of rad s^-1. Must be a
-        non-negative float.
-
-    Returns
-    -------
-    float
-        The Debye spectral density at frequency omega, in units of
-        rad s^-1.
-    """
-
-    assert cutoff_freq >= 0., (
-        'The cutoff freq must be a non-negative float, in units of rad s^-1')
-    assert scale_factor > 0., (
-        'The scaling factor must be a positive float, in units of rad s^-1')
-
-    if omega <= 0 or cutoff_freq == 0:
-        # Zero if omega < 0 as an asymmetric spectral density used.
-        # Zero if omega = 0 or cutoff = 0 to avoid DivideByZero error.
-        return 0.
-    return 2 * scale_factor * omega * cutoff_freq / (omega**2 + cutoff_freq**2)
-
-def bose_einstein_distrib(omega: float, temperature: float):
-
-    """
-    Calculates the Bose-Einstein distribution between 2 states i
-    and j, where omega = omega_i - omega_j. It is given by:
-
-    .. math::
-        n( \\omega )
-            = \\frac{1}{exp(\\hbar \\omega / k_B T) - 1}
-
-    Parameters
-    ----------
-    omega : float
-        The frequency gap between eigenstates, in units of
-        rad s^-1.
-    temperature : float
-        The temperature at which the Bose-Einstein distribution
-        should be evaluated, in units of Kelvin.
-
-    Returns
-    -------
-    float
-        The Bose-Einstein distribution between the 2 states
-        separated in energy by frequency omega.
-        A dimensionless quantity.
-    """
-
-    assert temperature > 0., (
-        'The temperature must be a positive float, in units of Kelvin')
-
-    if omega == 0.:
-        return 0.  # avoids DivideByZero error.
-    return 1. / (np.exp(constants.hbar * omega
-                        / (constants.k * temperature)) - 1)
