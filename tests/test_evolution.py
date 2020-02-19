69c0989bca694e530b32bdb397e0e8ad15b2f580
Joe Abbott <jwa7@uw.edu>
2020-02-19 18:45:20 +0000

    test to check consistent number of timesteps between dynamics models

--- tests/test_evolution.py
+++ tests/test_evolution.py
@@ -2,13 +2,28 @@
 
 import numpy as np
 import pytest
+from itertools import product
 
 from quantum_heom import evolution as evo
 from quantum_heom import utilities as util
 from quantum_heom.quantum_system import QuantumSystem
 
 
-def test_time_evolution_length():
+@pytest.mark.parametrize(
+    'dynamics, timesteps',
+    [('local dephasing lindblad', 1),
+     ('local dephasing lindblad', 100),
+     ('local dephasing lindblad', 250),
+     ('global thermalising lindblad', 1),
+     ('global thermalising lindblad', 100),
+     ('global thermalising lindblad', 250),
+     ('local thermalising lindblad', 1),
+     ('local thermalising lindblad', 100),
+     ('local thermalising lindblad', 250),
+     ('HEOM', 1),
+     ('HEOM', 100),
+     ('HEOM', 250)])
+def test_time_evolution_length(dynamics, timesteps):
 
     """
     Tests that the the density matrix is evaluated for the correct
@@ -18,13 +33,36 @@ def test_time_evolution_length():
     approaches.
     """
 
-def test_time_evolution_init_density_matrix():
+    qsys = QuantumSystem(sites=2, interaction_model='spin-boson',
+                         dynamics_model=dynamics, timesteps=timesteps)
+    assert len(qsys.time_evolution) == timesteps + 1
+
+
+@pytest.mark.parametrize(
+    'dynamics, timesteps',
+    [('local dephasing lindblad', 1),
+     ('local dephasing lindblad', 100),
+     ('local dephasing lindblad', 250),
+     ('global thermalising lindblad', 1),
+     ('global thermalising lindblad', 100),
+     ('global thermalising lindblad', 250),
+     ('local thermalising lindblad', 1),
+     ('local thermalising lindblad', 100),
+     ('local thermalising lindblad', 250),
+     ('HEOM', 1),
+     ('HEOM', 100),
+     ('HEOM', 250)])
+def test_time_evolution_init_density_matrix(dynamics, timesteps):
 
     """
     Tests that the first density matrix in the returned time
     evolution is the initial density matrix, for both Lindblad
     and HEOM approaches.
     """
+
+    qsys = QuantumSystem(sites=2, interaction_model='spin-boson',
+                         dynamics_model=dynamics, timesteps=timesteps)
+    assert np.all(qsys.time_evolution[0][1] == qsys.initial_density_matrix)
 
 
 @pytest.mark.parametrize(
@@ -78,7 +116,4 @@ def test_time_evo_lindblad_trace_one(dims, interactions, dynamics):
     evol = qsys.time_evolution
     for idx, step in enumerate(evol):
         trace = np.absolute(np.trace(step[1]))
-        # try:
         assert np.isclose(trace, 1.)
-        # except AssertionError:
-        #     import pdb; pdb.set_trace()
