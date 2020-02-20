bcd1b342d7efce41c880703d40d6f75d88e8a73b
Joe Abbott <jwa7@uw.edu>
2020-02-20 11:57:21 +0000

    module to test metadata fxns

---
+++ test_meta_data.py
@@ -0,0 +1,25 @@
+"""Tests the functions in metadata.py"""
+
+import pytest
+
+from quantum_heom import metadata as meta
+from quantum_heom.quantum_system import QuantumSystem
+
+
+@pytest.mark.parametrize(
+    'sites, interactions, dynamics',
+    [(2, 'spin-boson', 'local dephasing lindblad'),
+     (2, 'spin-boson', 'global thermalising lindblad'),
+     (2, 'spin-boson', 'local thermalising lindblad'),
+     (2, 'spin-boson', 'HEOM')])
+def test_integrated_trace_distance_zero(sites, interactions, dynamics):
+
+    """
+    Tests that the integrated trace distance of a system with
+    respect to itself is zero.
+    """
+
+    qsys = QuantumSystem(sites=sites, interaction_model=interactions,
+                         dynamics_model=dynamics)
+
+    assert meta.integrated_trace_distance([qsys], qsys) == [0]
